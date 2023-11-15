809a5b1dd382388bb5c827321bc56e8f36b5328f
HutaroZ12 <146780983+HutaroZ12@users.noreply.github.com>
2023-11-14 23:48:52 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -682,13 +682,13 @@ class TitleState extends MusicBeatState
 				// credTextShit.text = "Friday";
 				// credTextShit.screenCenter();
 				case 14:
-					addMoreText('Friday');
+					addMoreText('Star');
 				// credTextShit.visible = true;
 				case 15:
-					addMoreText('Night');
+					addMoreText('Funkin');
 				// credTextShit.text += '\nNight';
 				case 16:
-					addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
+					addMoreText('Ow Yeeah'); // credTextShit.text += '\nFunkin';
 
 				case 17:
 					skipIntro();
